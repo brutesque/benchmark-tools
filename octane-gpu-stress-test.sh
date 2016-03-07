61e03fb2d37cf5f50172acb88f919159dcf6eee4
Brutesque <alain@brutesque.com>
2016-03-07 02:41:57 +0100

    removed uncommented code

--- octane-gpu-stress-test.sh
+++ octane-gpu-stress-test.sh
@@ -12,7 +12,6 @@ while true
 do
 	sleep 1
 
-#	/Applications/OctaneBench\ 2.17.app/Contents/MacOS/octane_demo --benchmark -g 2 -a ~/benchmark2.txt && tail -n 1 ~/benchmark2.txt
 	## Testing all gpu's with octanebench
 	/Applications/OctaneBench\ 2.17.app/Contents/MacOS/octane_demo --benchmark -a /tmp/benchmark-octane.txt && tail -n 1 /tmp/benchmark-octane.txt
 done
